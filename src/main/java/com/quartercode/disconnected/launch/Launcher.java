abae3bded31125952722dd11873d846303281b61
LoadingByte <loadingbyte@gmail.com>
2013-07-16 10:18:07 +0200

    Launcher API Fix
    Fixed: Forgot to make the launcher vm arguments unmodifiable.

--- src/main/java/com/quartercode/disconnected/launch/Launcher.java
+++ src/main/java/com/quartercode/disconnected/launch/Launcher.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -110,7 +111,7 @@ public class Launcher {
      */
     public List<String> getVmArguments() {
 
-        return vmArguments;
+        return Collections.unmodifiableList(vmArguments);
     }
 
     /**
