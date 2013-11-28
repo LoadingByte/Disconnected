3b8e17b8164ab9911111716fe91d51decfef313e
LoadingByte <loadingbyte@gmail.com>
2013-11-28 09:55:12 +0100

    Equals() etc. for the AbstractFeatureDefinition
    Added: The abstract feature definition class now has hashCode(), equals(), toInfoString() and toString().

--- src/main/java/com/quartercode/disconnected/mocl/base/def/AbstractFeatureDefinition.java
+++ src/main/java/com/quartercode/disconnected/mocl/base/def/AbstractFeatureDefinition.java
@@ -21,6 +21,7 @@ package com.quartercode.disconnected.mocl.base.def;
 import com.quartercode.disconnected.mocl.base.Feature;
 import com.quartercode.disconnected.mocl.base.FeatureDefinition;
 import com.quartercode.disconnected.mocl.base.FeatureHolder;
+import com.quartercode.disconnected.util.InfoString;
 
 /**
  * An abstract feature definition is used to get a {@link Feature} from a {@link FeatureHolder}.
@@ -32,7 +33,7 @@ import com.quartercode.disconnected.mocl.base.FeatureHolder;
  * @see FeatureDefinition
  * @see Feature
  */
-public abstract class AbstractFeatureDefinition<F extends Feature> implements FeatureDefinition<F> {
+public abstract class AbstractFeatureDefinition<F extends Feature> implements FeatureDefinition<F>, InfoString {
 
     private final String name;
 
@@ -50,6 +51,50 @@ public abstract class AbstractFeatureDefinition<F extends Feature> implements Fe
     public String getName() {
 
         return name;
+    }
+
+    @Override
+    public int hashCode() {
+
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (name == null ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        AbstractFeatureDefinition<?> other = (AbstractFeatureDefinition<?>) obj;
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toInfoString() {
+
+        return "def " + name;
+    }
+
+    @Override
+    public String toString() {
+
+        return getClass().getName() + " [" + toInfoString() + "]";
     }
 
 }
