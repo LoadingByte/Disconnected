e95b7eb511a980da21ba6047fb970ce554e56294
LoadingByte <loadingbyte@gmail.com>
2013-12-29 10:40:30 +0100

    Patched Version to MoCl
    Added: The version object now uses the MoCl system.

--- src/main/java/com/quartercode/disconnected/world/comp/Version.java
+++ src/main/java/com/quartercode/disconnected/world/comp/Version.java
@@ -18,129 +18,268 @@
 
 package com.quartercode.disconnected.world.comp;
 
-import javax.xml.bind.annotation.XmlAttribute;
+import java.util.Arrays;
 import org.apache.commons.lang.Validate;
+import com.quartercode.disconnected.mocl.base.FeatureDefinition;
+import com.quartercode.disconnected.mocl.base.FeatureHolder;
+import com.quartercode.disconnected.mocl.base.def.AbstractFeatureDefinition;
+import com.quartercode.disconnected.mocl.base.def.DefaultFeatureHolder;
+import com.quartercode.disconnected.mocl.extra.FunctionDefinition;
+import com.quartercode.disconnected.mocl.extra.FunctionExecutor;
+import com.quartercode.disconnected.mocl.extra.Lockable;
+import com.quartercode.disconnected.mocl.extra.StopExecutionException;
+import com.quartercode.disconnected.mocl.extra.def.LockableFEWrapper;
+import com.quartercode.disconnected.mocl.extra.def.ObjectProperty;
+import com.quartercode.disconnected.mocl.util.FunctionDefinitionFactory;
+import com.quartercode.disconnected.mocl.util.PropertyAccessorFactory;
 
 /**
- * This class represents a simple version.
+ * This class represents a simple version of a {@link ComputerPart}.
  * A version contains a major version, minor version and a patch level.
+ * 
+ * @see ComputerPart
  */
-public class Version {
+public class Version extends DefaultFeatureHolder {
 
-    @XmlAttribute
-    private int major;
-    @XmlAttribute
-    private int minor;
-    @XmlAttribute
-    private int patchLevel;
+    // ----- Properties -----
 
     /**
-     * Creates a new version object using default values (0, 0 and 0).
+     * The major version component which is changed after very large changes.
+     */
+    protected static final FeatureDefinition<ObjectProperty<Integer>> MAJOR;
+
+    /**
+     * The minor version component which is changed if there are new features.
+     */
+    protected static final FeatureDefinition<ObjectProperty<Integer>> MINOR;
+
+    /**
+     * The revision version component which is changed after fixes.
+     */
+    protected static final FeatureDefinition<ObjectProperty<Integer>> REVISION;
+
+    static {
+
+        MAJOR = new AbstractFeatureDefinition<ObjectProperty<Integer>>("major") {
+
+            @Override
+            public ObjectProperty<Integer> create(FeatureHolder holder) {
+
+                return new ObjectProperty<Integer>(getName(), holder);
+            }
+
+        };
+
+        MINOR = new AbstractFeatureDefinition<ObjectProperty<Integer>>("minor") {
+
+            @Override
+            public ObjectProperty<Integer> create(FeatureHolder holder) {
+
+                return new ObjectProperty<Integer>(getName(), holder);
+            }
+
+        };
+
+        REVISION = new AbstractFeatureDefinition<ObjectProperty<Integer>>("revision") {
+
+            @Override
+            public ObjectProperty<Integer> create(FeatureHolder holder) {
+
+                return new ObjectProperty<Integer>(getName(), holder);
+            }
+
+        };
+
+    }
+
+    // ----- Properties End -----
+
+    // ----- Functions -----
+
+    /**
+     * Returns the major version component which is changed after very large changes.
+     */
+    public static final FunctionDefinition<Integer>                   GET_MAJOR;
+
+    /**
+     * Changes the major version component which is changed after very large changes.
+     * 
+     * <table>
+     * <tr>
+     * <th>Index</th>
+     * <th>Type</th>
+     * <th>Parameter</th>
+     * <th>Description</th>
+     * </tr>
+     * <tr>
+     * <td>0</td>
+     * <td>{@link Integer}</td>
+     * <td>major</td>
+     * <td>The new major version component.</td>
+     * </tr>
+     * </table>
+     */
+    public static final FunctionDefinition<Void>                      SET_MAJOR;
+
+    /**
+     * Returns the minor version component which is changed if there are new features.
+     */
+    public static final FunctionDefinition<Integer>                   GET_MINOR;
+
+    /**
+     * Changes the minor version component which is changed if there are new features.
+     * 
+     * <table>
+     * <tr>
+     * <th>Index</th>
+     * <th>Type</th>
+     * <th>Parameter</th>
+     * <th>Description</th>
+     * </tr>
+     * <tr>
+     * <td>0</td>
+     * <td>{@link Integer}</td>
+     * <td>minor</td>
+     * <td>The new minor version component.</td>
+     * </tr>
+     * </table>
+     */
+    public static final FunctionDefinition<Void>                      SET_MINOR;
+
+    /**
+     * Returns the revision version component which is changed after fixes.
+     */
+    public static final FunctionDefinition<Integer>                   GET_REVISION;
+
+    /**
+     * Changes the revision version component which is changed after fixes.
+     * 
+     * <table>
+     * <tr>
+     * <th>Index</th>
+     * <th>Type</th>
+     * <th>Parameter</th>
+     * <th>Description</th>
+     * </tr>
+     * <tr>
+     * <td>0</td>
+     * <td>{@link Integer}</td>
+     * <td>revision</td>
+     * <td>The new revision version component.</td>
+     * </tr>
+     * </table>
+     */
+    public static final FunctionDefinition<Void>                      SET_REVISION;
+
+    /**
+     * Changes the stored version to the ones stored in the given version object.
+     * 
+     * <table>
+     * <tr>
+     * <th>Index</th>
+     * <th>Type</th>
+     * <th>Parameter</th>
+     * <th>Description</th>
+     * </tr>
+     * <tr>
+     * <td>0</td>
+     * <td>{@link Version}</td>
+     * <td>version</td>
+     * <td>The version object to get the new version from.</td>
+     * </tr>
+     * </table>
+     */
+    public static final FunctionDefinition<Void>                      FROM_OBJECT;
+
+    /**
+     * Changes the stored version to the ones stored in the given version string.
+     * The string is using the format MAJOR.MINOR.PATCHLEVEL (e.g. 1.2.5).
+     * 
+     * <table>
+     * <tr>
+     * <th>Index</th>
+     * <th>Type</th>
+     * <th>Parameter</th>
+     * <th>Description</th>
+     * </tr>
+     * <tr>
+     * <td>0</td>
+     * <td>{@link String}</td>
+     * <td>version</td>
+     * <td>The new version to parse.</td>
+     * </tr>
+     * </table>
+     */
+    public static final FunctionDefinition<Void>                      FROM_STRING;
+
+    /**
+     * Returns the stored version as a string.
+     * The string is using the format MAJOR.MINOR.PATCHLEVEL (e.g. 1.2.5).
+     */
+    public static final FunctionDefinition<String>                    TO_STRING;
+
+    static {
+
+        GET_MAJOR = FunctionDefinitionFactory.create("getMajor", Version.class, PropertyAccessorFactory.createGet(MAJOR));
+        SET_MAJOR = FunctionDefinitionFactory.create("setMajor", Version.class, new LockableFEWrapper<Void>(PropertyAccessorFactory.createSet(MAJOR)), Integer.class);
+
+        GET_MINOR = FunctionDefinitionFactory.create("getMinor", Version.class, PropertyAccessorFactory.createGet(MINOR));
+        SET_MINOR = FunctionDefinitionFactory.create("setMinor", Version.class, new LockableFEWrapper<Void>(PropertyAccessorFactory.createSet(MINOR)), Integer.class);
+
+        GET_REVISION = FunctionDefinitionFactory.create("getRevision", Version.class, PropertyAccessorFactory.createGet(REVISION));
+        SET_REVISION = FunctionDefinitionFactory.create("setRevision", Version.class, new LockableFEWrapper<Void>(PropertyAccessorFactory.createSet(REVISION)), Integer.class);
+
+        FROM_OBJECT = FunctionDefinitionFactory.create("fromObject", Version.class, new FunctionExecutor<Void>() {
+
+            @Override
+            @Lockable
+            public Void invoke(FeatureHolder holder, Object... arguments) throws StopExecutionException {
+
+                holder.get(SET_MAJOR).invoke( ((Version) arguments[0]).get(GET_MAJOR).invoke());
+                holder.get(SET_MINOR).invoke( ((Version) arguments[0]).get(GET_MINOR).invoke());
+                holder.get(SET_REVISION).invoke( ((Version) arguments[0]).get(GET_REVISION).invoke());
+
+                return null;
+            }
+
+        }, Version.class);
+
+        FROM_STRING = FunctionDefinitionFactory.create("fromString", Version.class, new FunctionExecutor<Void>() {
+
+            @Override
+            @Lockable
+            public Void invoke(FeatureHolder holder, Object... arguments) throws StopExecutionException {
+
+                String[] versionParts = ((String) arguments[0]).split("\\.");
+                Validate.isTrue(versionParts.length == 3, "The version string must be splitted in 3 parts by dots (e.g. 1.2.5): ", Arrays.toString(versionParts));
+                holder.get(SET_MAJOR).invoke(Integer.parseInt(versionParts[0]));
+                holder.get(SET_MINOR).invoke(Integer.parseInt(versionParts[1]));
+                holder.get(SET_REVISION).invoke(Integer.parseInt(versionParts[2]));
+
+                return null;
+            }
+
+        }, String.class);
+
+        TO_STRING = FunctionDefinitionFactory.create("toString", Version.class, new FunctionExecutor<String>() {
+
+            @Override
+            public String invoke(FeatureHolder holder, Object... arguments) throws StopExecutionException {
+
+                return holder.get(GET_MAJOR).invoke() + "." + holder.get(GET_MINOR).invoke() + "." + holder.get(GET_REVISION).invoke();
+            }
+
+        });
+
+    }
+
+    // ----- Functions End -----
+
+    /**
+     * Creates a new version object.
      */
     public Version() {
 
-    }
-
-    /**
-     * Creates a new version object and sets the values to the given ones.
-     * 
-     * @param major The major version which is changed after very large changes.
-     * @param minor The minor version which is changed if there are new features.
-     * @param patchLevel The patch level which is changed after fixes.
-     */
-    public Version(int major, int minor, int patchLevel) {
-
-        this.major = major;
-        this.minor = minor;
-        this.patchLevel = patchLevel;
-    }
-
-    /**
-     * Creates a new version object and sets the values by parsing a version string.
-     * This is using the format MAJOR.MINOR.PATCHLEVEL (e.g. 1.2.5).
-     * 
-     * @param version The version string to parse the values from.
-     */
-    public Version(String version) {
-
-        String[] versionParts = version.split("\\.");
-        Validate.isTrue(versionParts.length == 3, "The version string must be splitted in 3 parts by dots (e.g. 1.2.5): ", version);
-
-        major = Integer.parseInt(versionParts[0]);
-        minor = Integer.parseInt(versionParts[1]);
-        patchLevel = Integer.parseInt(versionParts[2]);
-    }
-
-    /**
-     * Returns the major version which is changed after very large changes.
-     * 
-     * @return The major version.
-     */
-    public int getMajor() {
-
-        return major;
-    }
-
-    /**
-     * Returns the minor version which is changed if there are new features.
-     * 
-     * @return The minor version.
-     */
-    public int getMinor() {
-
-        return minor;
-    }
-
-    /**
-     * Returns the patch level which is changed after fixes.
-     * 
-     * @return The patch level.
-     */
-    public int getPatchLevel() {
-
-        return patchLevel;
-    }
-
-    @Override
-    public int hashCode() {
-
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + major;
-        result = prime * result + minor;
-        result = prime * result + patchLevel;
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        Version other = (Version) obj;
-        if (major != other.major) {
-            return false;
-        }
-        if (minor != other.minor) {
-            return false;
-        }
-        if (patchLevel != other.patchLevel) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-
-        return major + "." + minor + "." + patchLevel;
     }
 
 }
