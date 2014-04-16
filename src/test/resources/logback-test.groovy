f68a9d2f25064a6de5ef9a8d10071e785cc0d66b
LoadingByte <loadingbyte@gmail.com>
2014-04-16 08:53:44 +0200

    Remove unnecessary logback-test.groovy
    Remove: Since logback does not provide support for a logback-test.groovy, the file is not necessary.

--- src/test/resources/logback-test.groovy
+++
@@ -1,7 +0,0 @@
-// Console appender
-appender("STDOUT", ConsoleAppender) {
-    encoder(PatternLayoutEncoder) { pattern = "%date{HH:mm:ss.SSS} %level ['%thread' %file:%line] - %msg%n" }
-}
-
-// Every test has the same logging settings
-root(INFO, ["STDOUT"])
