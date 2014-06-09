99f00a6aa7dae31c8d118447cce278408b007374
LoadingByte <loadingbyte@gmail.com>
2014-06-09 13:01:32 +0200

    Fix missing JAXB context path entry issue
    The JAXB context path now contains the
    com.quartercode.disconnected.world.general which was previously missing.

--- src/main/java/com/quartercode/disconnected/Main.java
+++ src/main/java/com/quartercode/disconnected/Main.java
@@ -283,6 +283,7 @@ public class Main {
         GlobalStorage.put("contextPath", "com.quartercode.disconnected.world.comp.net");
         GlobalStorage.put("contextPath", "com.quartercode.disconnected.world.comp.os");
         GlobalStorage.put("contextPath", "com.quartercode.disconnected.world.comp.program");
+        GlobalStorage.put("contextPath", "com.quartercode.disconnected.world.general");
 
         GlobalStorage.put("themes", Main.class.getResource("/ui/default/default.xml"));
         GlobalStorage.put("themes", Main.class.getResource("/ui/desktop/desktop.xml"));
