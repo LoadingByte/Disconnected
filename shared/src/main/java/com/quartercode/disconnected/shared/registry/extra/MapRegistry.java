b290a32ea8cc1ffd8c9915c7ecbf1dbff270ded9
LoadingByte <loadingbyte@gmail.com>
2014-11-02 11:22:53 +0100

    Add MappedValueRegistry (and impl) for BiMap registries

---
+++ shared/src/main/java/com/quartercode/disconnected/shared/registry/extra/MapRegistry.java
@@ -0,0 +1,186 @@
+/*
+ * This file is part of Disconnected.
+ * Copyright (c) 2013 QuarterCode <http://www.quartercode.com/>
+ *
+ * Disconnected is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Disconnected is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Disconnected. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.quartercode.disconnected.shared.registry.extra;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * A {@link MappedValueRegistry} that uses two {@link HashMap}s as internal data structures.
+ * 
+ * @param <L> The type of the left values, which are mapped to the right values.
+ * @param <R> The type of the right values, which are mapped to the left values.
+ * @see MappedValueRegistry
+ */
+public class MapRegistry<L, R> implements MappedValueRegistry<L, R> {
+
+    private final Map<L, R>     leftToRight = new HashMap<>();
+    private final Map<R, L>     rightToLeft = new HashMap<>();
+    private List<Mapping<L, R>> listCache;
+
+    @Override
+    public L getLeft(R right) {
+
+        return rightToLeft.get(right);
+    }
+
+    @Override
+    public R getRight(L left) {
+
+        return leftToRight.get(left);
+    }
+
+    @Override
+    public List<Mapping<L, R>> getValues() {
+
+        // Update the list cache
+        if (listCache == null) {
+            listCache = new ArrayList<>();
+            for (Entry<L, R> entry : leftToRight.entrySet()) {
+                listCache.add(new DefaultMapping<>(entry.getKey(), entry.getValue()));
+            }
+            listCache = Collections.unmodifiableList(listCache);
+        }
+
+        return listCache;
+    }
+
+    @Override
+    public void addMapping(L left, R right) {
+
+        // Clear any old mappings with the same left or right value
+        removeLeft(left);
+        removeRight(right);
+
+        // Add the new mapping to both maps
+        leftToRight.put(left, right);
+        rightToLeft.put(right, left);
+
+        // Invalidate the list cache
+        listCache = null;
+    }
+
+    @Override
+    public void addValue(Mapping<L, R> value) {
+
+        addMapping(value.getLeft(), value.getRight());
+    }
+
+    @Override
+    public void removeLeft(L left) {
+
+        rightToLeft.remove(getRight(left));
+        leftToRight.remove(left);
+
+        // Invalidate the list cache
+        listCache = null;
+    }
+
+    @Override
+    public void removeRight(R right) {
+
+        leftToRight.remove(getLeft(right));
+        rightToLeft.remove(right);
+
+        // Invalidate the list cache
+        listCache = null;
+    }
+
+    @Override
+    public void removeValue(Mapping<L, R> value) {
+
+        removeLeft(value.getLeft());
+        removeRight(value.getRight());
+    }
+
+    public static class DefaultMapping<L, R> implements Mapping<L, R> {
+
+        private final L left;
+        private final R right;
+
+        public DefaultMapping(L left, R right) {
+
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public L getLeft() {
+
+            return left;
+        }
+
+        @Override
+        public L getKey() {
+
+            return left;
+        }
+
+        @Override
+        public String getName() {
+
+            return left instanceof NamedValue ? ((NamedValue) left).getName() : String.valueOf(left);
+        }
+
+        @Override
+        public R getRight() {
+
+            return right;
+        }
+
+        @Override
+        public R getValue() {
+
+            return right;
+        }
+
+        @Override
+        public R setValue(R value) {
+
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int hashCode() {
+
+            return HashCodeBuilder.reflectionHashCode(this);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            return EqualsBuilder.reflectionEquals(this, obj);
+        }
+
+        @Override
+        public String toString() {
+
+            return ToStringBuilder.reflectionToString(this);
+        }
+
+    }
+
+}
---
+++ shared/src/main/java/com/quartercode/disconnected/shared/registry/extra/MappedValueRegistry.java
@@ -0,0 +1,108 @@
+/*
+ * This file is part of Disconnected.
+ * Copyright (c) 2013 QuarterCode <http://www.quartercode.com/>
+ *
+ * Disconnected is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Disconnected is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Disconnected. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.quartercode.disconnected.shared.registry.extra;
+
+import java.util.Map;
+import com.quartercode.disconnected.shared.registry.extra.MappedValueRegistry.Mapping;
+
+/**
+ * A mapped value registry is a {@link MultipleValueRegistry} that maps left values to right values (it is a simple map).
+ * However, the mappings can be retrieved bidirectionally using {@link #getLeft(Object)} and {@link #getRight(Object)}.
+ * New mappings can be added using {@link #addMapping(Object, Object)}.
+ * Old ones can be removed with {@link #removeLeft(Object)} and {@link #removeRight(Object)}.
+ * Internally, a mapped value registry is just a multiple value registry that stores {@link Mapping}s instead of custom objects.<br>
+ * <br>
+ * If one of the two value types is or should be handled as a {@link NamedValue}, it should be used as the left one.
+ * Mappings implement the named value interface and redirect the named value call to the left object.
+ * 
+ * @param <L> The type of the left values, which are mapped to the right values.
+ * @param <R> The type of the right values, which are mapped to the left values.
+ * @see MultipleValueRegistry
+ * @see Mapping
+ */
+public interface MappedValueRegistry<L, R> extends MultipleValueRegistry<Mapping<L, R>> {
+
+    /**
+     * Returns the left mapping for the given right object.
+     * 
+     * @param right The right object whose left partner should be returned.
+     * @return The assigned left partner of the given right object.
+     */
+    public L getLeft(R right);
+
+    /**
+     * Returns the right mapping for the given left object.
+     * 
+     * @param left The left object whose right partner should be returned.
+     * @return The assigned right partner of the given left object.
+     */
+    public R getRight(L left);
+
+    /**
+     * Adds the given mapping in order to make it available through the accessor methods.
+     * Note that all mappings with the same left <b>or</b> right object are removed.
+     * 
+     * @param left The left object that should be mapped to the given right object.
+     * @param right The right object that should be mapped to the given left object.
+     */
+    public void addMapping(L left, R right);
+
+    /**
+     * Removes the mapping that maps the given left object to some right object.
+     * 
+     * @param left The left object whose mapping should be removed.
+     */
+    public void removeLeft(L left);
+
+    /**
+     * Removes the mapping that maps the given right object to some left object.
+     * 
+     * @param right The right object whose mapping should be removed.
+     */
+    public void removeRight(R right);
+
+    /**
+     * An internal class that is used by {@link MappedValueRegistry} implementations to act as {@link MultipleValueRegistry}s.
+     * The custom object type of the multiple value registry is replaced with such a mapping type.
+     * Note that the all {@link #getName()} calls on a mapping object are redirected to the left object.
+     * See the main mapped value registry class for more details.
+     * 
+     * @param <L> The type of the left value, which is mapped to the right value.
+     * @param <R> The type of the right value, which is mapped to the left value.
+     * @see MappedValueRegistry
+     */
+    public static interface Mapping<L, R> extends Map.Entry<L, R>, NamedValue {
+
+        /**
+         * Returns the left value, which is mapped to the right value.
+         * 
+         * @return The left value.
+         */
+        public L getLeft();
+
+        /**
+         * Returns the right value, which is mapped to the left value.
+         * 
+         * @return The right value.
+         */
+        public R getRight();
+
+    }
+
+}
